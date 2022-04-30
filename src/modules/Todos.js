7e8ee1249f1979116273aecd05ad4ff33a2040c4
enajrevir <enajrevir@gmail.com>
2022-04-30 19:43:40 +0900

    Todos 리듀서 함수 만들기

--- src/modules/Todos.js
+++ src/modules/Todos.js
@@ -1,10 +1,10 @@
 // 액션 타입 정의하기
-export const input = () => ({ type: INPUT })
 export const changeInput = () => ({ type: CHANGEINPUT })
 export const insert = () => ({ type: INSERT })
 export const toggle = () => ({ type: TOGGLE })
 export const remove = () => ({ type: REMOVE })
 
+// 액션 생성 함수
 export const changeInput = input => ({
   type: CHANGE_INPUT,
   input
@@ -29,3 +29,52 @@ export const remove = id => ({
   type: REMOVE,
   id
 });
+
+// 초기 상태
+const initialState = {
+  input: '',
+  todos: [
+    {
+      id: 1,
+      text: '리덕스 기초 배우기',
+      done: true
+    },
+    {
+      id: 2,
+      text: '리액트와 리덕스 사용하기',
+      done: false
+    },
+  ],
+}
+
+// 리듀서 함수
+function todos(state = initialState, action) {
+  switch(action.type) {
+    case CHANGE_INPUT:
+      return {
+        ...state,
+        input: action.input
+      }
+    case INSERT:
+      return {
+        ...state,
+        todos: state.todos.concat(action.todo)
+      }
+    case TOGGLE:
+      return {
+        ...state,
+        todos: state.todos.map(todo =>
+          todo.id === action.id ? { ...todo, done: !todo.done } : todo
+        )
+      }
+    case REMOVE:
+      return {
+        ...state,
+        todos: state.todos.filter(todo => todo.id !== action.id)
+      };
+    default:
+      return state;
+  }
+}
+
+export default todos;
