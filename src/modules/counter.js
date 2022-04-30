e5970844a81bd5d6e6099b05f62efd93a2317483
enajrevir <enajrevir@gmail.com>
2022-05-01 07:29:28 +0900

    redux-action createAction 사용으로 액션생성 방식 바꾸기

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,10 +1,12 @@
+import { createAction } from 'redux-actions';
+
 // 액션 타입 정의하기
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
 // 액션 생성 함수 만들기
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 const initialState = {
   number: 0
