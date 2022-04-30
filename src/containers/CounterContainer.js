280b5a72550e7ddef5c57e24c5ce19239999c8af
enajrevir <enajrevir@gmail.com>
2022-04-30 21:48:23 +0900

    bindActionCreators 작업

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
@@ -9,20 +10,16 @@ const CounterContainer = ({ number, increase, decrease }) => {
   );
 };
 
-const mapStateToProps = state => ({
-  number: state.counter.number
-});
-
-const mapDispatchToProps = dispatch => ({
-  increase: () => {
-    dispatch(increase());
-  },
-  decrease: () => {
-    dispatch(decrease());
-  }
-});
-
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(CounterContainer);
+  state => ({
+    number: state.counter.number,
+  }),
+  dispatch =>
+    bindActionCreators(
+      {
+        increase,
+        decrease
+      },
+      dispatch
+    ),
+  )(CounterContainer);
